413185575cb79f7e814eadd3596561a0583334eb
atagunay <atafb2000gunay@gmail.com>
2023-12-12 17:05:58 +0000

    [feature] Add JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"state","l":"Complete"},{"p":"genetic","l":"GeneticAlgorithm"},{"p":"state","l":"Idle"},{"p":"<Unnamed>","l":"Main"},{"p":"state","l":"State"}];updateSearchResults();
